71034a1fb6a59321d0e12756f82ca1ca58f7b0d8
Napermial <hatvani9823@gmail.com>
2021-12-24 17:01:35 +0100

    experiments grid created - ready for backend ingestion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MainPage from "./index/MainPage";
+import Experiments from "./experiments/Experiments";
 
 
 function App() {
     return (
         <div className="App">
             <MainPage/>
+            <Experiments/>
         </div>
     );
 }
